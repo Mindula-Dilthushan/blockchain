e78529cc57d80491ac6cc1803f390ef98bcb489e
Mindula Dilthushan Manamperi <minduladilthushan1@gmail.com>
2022-04-11 07:09:51 +0530

    install-crypto-js

--- main.js
+++ main.js
@@ -4,3 +4,24 @@
  * minduladilthushan1@gmail.com
  * 22-04-11
  */
+
+const SHA256 = require('crypto-js/sha256')
+
+class MyBlock {
+
+    constructor(index, timestamp, data, previousHash = '') {
+
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = '';
+
+    }
+
+    calculateHash(){
+
+
+
+    }
+}
